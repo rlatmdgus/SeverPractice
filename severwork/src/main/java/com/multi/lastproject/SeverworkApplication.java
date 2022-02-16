0e41717f08e7c21d9c92b8aa485394ba01e6d43f
rlatmdgus <gha0810@naver.com>
2022-02-16 17:21:23 +0900

    Initial commit

---
+++ severwork/src/main/java/com/multi/lastproject/SeverworkApplication.java
@@ -0,0 +1,13 @@
+package com.multi.lastproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeverworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeverworkApplication.class, args);
+	}
+
+}
